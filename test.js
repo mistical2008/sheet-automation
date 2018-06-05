911c65a1db74cf7ebe2e2002a03af110a15bcc3e
Evgeniy Talko <etalko@gmail.com>
2018-06-05 17:11:45 +0300

    Some code stuff

---
+++ test.js
@@ -0,0 +1,11 @@
+let keywords1 = [];
+
+function greet(phrase,arr) {
+  // console.log(phrase);
+  arr.push(phrase);
+}
+
+greet("Hello!",keywords1);
+greet("Yay!",keywords1);
+greet("Wow!",keywords1);
+console.log(keywords1);
