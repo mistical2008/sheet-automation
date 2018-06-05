69c920bd43703e1122ca54fb4c48cd1b82ac5da0
Evgeniy Talko <etalko@gmail.com>
2018-06-05 15:12:27 +0300

    refactoring function code

--- heading-parse-test.js
+++ heading-parse-test.js
@@ -1,16 +1,21 @@
 var fs = require('fs');
 filename = "./heding_test.txt"
-fs.readFile(filename, 'utf8', function(err, data) {
-  if (err) throw err;
-  console.log('OK: ' + filename);
-  // Parse function
-  const parsedParagraph = data.match(/\n{(.*?)}\n/g).map(val => {
-    return val.replace(/\n/g, '');
-  });
 
-  // output outcome
-  console.log('Text output:\n'.toUpperCase());
-  console.log(data);
-  console.log('Parsed paragraph:\n'.toUpperCase());
-  console.log(parsedParagraph);
-});
+function getHeadingParts(file,) {
+  fs.readFile(file, 'utf8', function(err, data) {
+    if (err) throw err;
+    console.log('OK: ' + file);
+    // Parse function
+    const parsedParagraph = data.match(/\{(.*?)\}/g).map(val => {
+      return val.replace(/[\{\}]/g, '');
+    });
+    // output outcome
+    console.log('Text output:\n'.toUpperCase());
+    console.log(data);
+    console.log('Parsed paragraph:\n'.toUpperCase());
+    console.log(parsedParagraph);
+  });
+};
+
+// return getHeadingParts(filename);
+const headingParts = getHeadingParts(filename);
