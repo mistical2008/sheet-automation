c01e3d90e6f720e4928ed0e86de88ccf3f698c45
Evgeniy Talko <etalko@gmail.com>
2018-06-05 18:18:27 +0300

    Some fixes

--- heading-parse-test.1.js
+++ heading-parse-test.1.js
@@ -4,33 +4,5 @@ let keywords1 = [];
 let keywords2 = [];
 
 // Parse function
-function getHeadingParts(file,index,arr) {
-  fs.readFile(file, 'utf8', function(err, data) {
-    if (err) throw err;
-    // console.log('OK: ' + file);
-    // Parse paragraph
-    var parsedParagraph = data.match(/\{(.*?)\}/g).map(val => {
-      return val.replace(/[\{\}]/g, '');
-    });
-
-    // Parse 1st part keywords
-    var parsedHeadingsKeywords = parsedParagraph[index].split('|');
-
-    console.log(parsedHeadingsKeywords);
-    console.log(parsedParagraph);
-    // console.log(keywords1);
-    // return parsedHeadingsKeywords;
-    // return parsedHeadingsKeywords;
-
-    function pushToArray(arr) {
-      parsedHeadingsKeywords.map(val => {
-        return arr.push(val);
-      });
-    };
-    pushToArray(arr);
-
-  });
-};
-
-var test = getHeadingParts(filename,0,keywords1);
-console.log(keywords1);
+var text = fs.readFileSync(filename,'utf8');
+console.log(text);
