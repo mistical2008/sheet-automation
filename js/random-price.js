3036e60e8aa99d2868051fd0bfb7dc175546a0ad
Evgeniy Talko <etalko@gmail.com>
2018-06-11 11:34:12 +0300

    Updated variables names to more informative

--- js/random-price.js
+++ js/random-price.js
@@ -1,9 +1,9 @@
-let init = 218;
-let til = 398;
+let priceMin = 218;
+let priceMax = 398;
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-let res = getRandomInt(init, til) * 10;
+let res = getRandomInt(priceMin, priceMax) * 10;
 console.log(res);
