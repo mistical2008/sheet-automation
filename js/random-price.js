d6ab94fd96cf5087dec0965268c9aed613566123
Evgeniy Talko <etalko@gmail.com>
2018-06-12 19:12:21 +0300

    renamed res to randPrice

--- js/random-price.js
+++ js/random-price.js
@@ -5,5 +5,5 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-let res = getRandomInt(priceMin, priceMax) * 10;
-console.log(res);
+let randPrice = getRandomInt(priceMin, priceMax) * 10;
+console.log(randPrice);
