0c8d5c1f8c58202dd5213a0ee730dfe307a914d3
Evgeniy Talko <etalko@gmail.com>
2018-06-13 19:15:04 +0300

    Moved multiply by 10 into function

--- js/random-price.js
+++ js/random-price.js
@@ -2,8 +2,7 @@ let priceMin = 218;
 let priceMax = 398;
 
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min) * 10;
 }
 
-let randPrice = getRandomInt(priceMin, priceMax) * 10;
-console.log(randPrice);
+let randPrice = getRandomInt(priceMin, priceMax);
