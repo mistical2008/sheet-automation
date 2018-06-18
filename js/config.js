60de3b4053ee8fc58d7a678ce5c568449c810d93
Evgeniy Talko <etalko@gmail.com>
2018-06-18 16:07:47 +0300

    Added config for variable export

---
+++ js/config.js
@@ -0,0 +1,8 @@
+// const brand = ('../testdir')
+const path = require('path');
+module.exports.brand = ("../testdir");
+module.exports.headingsFile = ('../heading_test.txt');
+module.exports.introText = ('../random-text.txt');
+module.exports.copyFile = ('../copy-text.txt');
+module.exports.priceMin = (218);
+module.exports.priceMax = (398);
