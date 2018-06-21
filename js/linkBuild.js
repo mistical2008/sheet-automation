a13aa7e1e25905b89249983975026ba55b4fc157
Evgeniy Talko <etalko@gmail.com>
2018-06-21 10:33:34 +0300

    Added initial DB build functions. Now DB with model, price, prodID

--- js/linkBuild.js
+++ js/linkBuild.js
@@ -1,15 +1,39 @@
-// Задаем переменные
+// Set variables
 // const fs = require('fs');
 const fs = require('fs');
 // const brand = ('../testdir');
 let config = require('./config');
 let workPath = config.workPath;
-const brand = fs.readdirSync(workPath)[0]; // Получаем папку бренда
-// const cleanBrand = workPath.replace(/.\/assets/, '');
-const modelsDirs = fs.readdirSync(workPath + '/' + brand); // Получаем папки моделей
+const brand = fs.readdirSync(workPath)[0]; // Get brand dir
+const modelsDirs = fs.readdirSync(workPath + '/' + brand); // Get models dirs
 let DB = [];
+let prodidAdd = config.prodidAdd;
+let priceAdd = config.priceAdd;
 
-// Генерация базы папок моделей и адресов изображений
+
+// Define dirname parse function
+function getFromDirName(key, toSearch, toReplace) {
+  return DB.map(obj => {
+    let index = DB.indexOf(obj);
+    let objKey = key;
+    return obj.modelDir.match(toSearch).map(val => {
+      DB[index][objKey] = val.replace(toReplace, '')
+    })
+  });
+}
+
+// Define change price/prodID function
+function changeDBKey(whatToChange, key) {
+  DB.map(obj => {
+    let index = DB.indexOf(obj);
+    let objKey = key;
+    DB[index][objKey] = (whatToChange != undefined)
+      ? Number(DB[index][objKey]) + whatToChange
+      : Number(DB[index][objKey]);
+  })
+}
+
+// Initial DB generate
 function DBgen() {
   modelsDirs.map(dir => {
     function createObj() {
@@ -27,5 +51,11 @@ function DBgen() {
     }
     DB.push(createObj());
   });
+  // Call functions for fill DB with model, price, prodID and change if needed
+  getFromDirName('model', /\w(.*?)\(/g, /\s\($/);
+  getFromDirName('price', /\)(.*?)\d$/g, /^\)\s/);
+  getFromDirName('prodID', /\((.*?)\)/g, /[\(\)]/g);
+  changeDBKey(prodidAdd, 'prodID');
+  changeDBKey(priceAdd, 'price');
 }
 DBgen();
